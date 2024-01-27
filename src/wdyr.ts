ca95ad76dd169ceea4624750f4daee3b2dd69041
Paul Puey <paul@edge.app>
2024-01-26 18:12:12 -0800

    Add why-did-you-render

---
+++ src/wdyr.ts
@@ -0,0 +1,11 @@
+import whyDidYouRender from '@welldone-software/why-did-you-render'
+import React from 'react'
+
+whyDidYouRender(React, {
+  onlyLogs: true,
+  titleColor: 'green',
+  diffNameColor: 'aqua',
+  logOnDifferentValues: false,
+  trackAllPureComponents: true,
+  trackHooks: true
+})
